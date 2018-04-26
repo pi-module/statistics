1362a66d4b09e48f15a9ceebebb190e9d396a1e4
voltan <djvoltan@gmail.com>
2018-04-26 11:49:20 +0430

    work on module

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -1,10 +1,12 @@
 CREATE TABLE `{log}` (
   `id`          INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `section`     ENUM ('front', 'admin', 'feed', 'api')      NOT NULL DEFAULT 'front',
   `module`      VARCHAR(50)      NOT NULL DEFAULT '',
   `entity`      VARCHAR(50)      NOT NULL DEFAULT '',
-  `entity_id`   INT(10)          NOT NULL DEFAULT '0',
+  `entity_id`   INT(10)    UNSIGNED       NOT NULL DEFAULT '0',
   `action`      VARCHAR(20)      NOT NULL DEFAULT '',
   `time_create` INT(10) UNSIGNED NOT NULL DEFAULT '0',
+  `uid` INT(10) UNSIGNED NOT NULL DEFAULT '0',
   `ip`          VARCHAR(16),
   `source`      VARCHAR(10),
   PRIMARY KEY (`id`),
@@ -33,25 +35,13 @@ CREATE TABLE `{total_monthly}` (
   PRIMARY KEY (`id`)
 );
 
-# CREATE TABLE `{module_hourly}` (
-#   `id`          INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-#   `date`        DATE                      DEFAULT NULL,
-#   `hour`        INT(11) UNSIGNED          DEFAULT NULL,
-#   `total_count` INT(11) UNSIGNED          DEFAULT NULL,
-#   `module`      VARCHAR(50)      NOT NULL DEFAULT '',
-#   `entity`      VARCHAR(50)      NOT NULL DEFAULT '',
-#   `entity_id`   INT(10)          NOT NULL DEFAULT '0',
-#   `action`      VARCHAR(20)      NOT NULL DEFAULT '',
-#   PRIMARY KEY (`id`)
-# );
-
 CREATE TABLE `{module_daily}` (
   `id`          INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `date`        DATE                      DEFAULT NULL,
   `total_count` INT(11) UNSIGNED          DEFAULT NULL,
   `module`      VARCHAR(50)      NOT NULL DEFAULT '',
   `entity`      VARCHAR(50)      NOT NULL DEFAULT '',
-  `entity_id`   INT(10)          NOT NULL DEFAULT '0',
+  `entity_id`   INT(10)    UNSIGNED       NOT NULL DEFAULT '0',
   `action`      VARCHAR(20)      NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
 );
@@ -62,7 +52,7 @@ CREATE TABLE `{module_monthly}` (
   `total_count` INT(11) UNSIGNED          DEFAULT NULL,
   `module`      VARCHAR(50)      NOT NULL DEFAULT '',
   `entity`      VARCHAR(50)      NOT NULL DEFAULT '',
-  `entity_id`   INT(10)          NOT NULL DEFAULT '0',
+  `entity_id`   INT(10)     UNSIGNED      NOT NULL DEFAULT '0',
   `action`      VARCHAR(20)      NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
 );
--- src/Api/Log.php
+++ src/Api/Log.php
@@ -17,20 +17,22 @@ use Pi;
 use Pi\Application\Api\AbstractApi;
 
 /**
- * Pi::api('log', 'statistics')->save($module, $entity, $entityId, $action, $source, $time);
+ * Pi::api('log', 'statistics')->save($module, $entity, $entityId, $options);
  */
 class Log extends AbstractApi
 {
-    public function save($module, $entity, $entityId, $action, $source = 'web', $time = null)
+    public function save($module, $entity, $entityId = 0, $options = [])
     {
         $log              = Pi::model('log', 'statistics')->createRow();
         $log->module      = $module;
         $log->entity      = $entity;
         $log->entity_id   = $entityId;
-        $log->action      = $action;
-        $log->time_create = $time ?: time();
-        $log->ip          = $_SERVER['REMOTE_ADDR'];
-        $log->source      = $source;
+        $log->action      = isset($options['action']) ? $options['action'] : 'hits';
+        $log->source      = isset($options['source']) ? $options['source'] : 'web';
+        $log->section     = isset($options['section']) ? $options['section'] : 'front';
+        $log->time_create = isset($options['time']) ? $options['time'] : time();
+        $log->ip          = isset($options['ip']) ? $options['ip'] : Pi::user()->getIp();
+        $log->uip         = isset($options['uid']) ? $options['uid'] : Pi::user()->getId();
         $log->save();
     }
 }
